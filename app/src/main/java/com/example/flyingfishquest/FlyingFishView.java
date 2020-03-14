6071522907684cba17be92cbc6373855d6aee268
Anushka Chandel <chandelanushka99@gmail.com>
2020-03-15 00:49:23 +0530

    Move & Fly

--- app/src/main/java/com/example/flyingfishquest/FlyingFishView.java
+++ app/src/main/java/com/example/flyingfishquest/FlyingFishView.java
@@ -11,15 +11,22 @@ import android.view.View;
 
 public class FlyingFishView extends View {
 
-    private Bitmap fish;
+    private Bitmap fish[]  = new Bitmap[2];
+    private int fishX =10;
+    private int fishY ;
+    private int fishSpeed;
+    private int canvasWidth, canvasHeight;
     private Bitmap backgroundImage;
     private Paint scorePaint = new Paint();
     private Bitmap life[] = new Bitmap[2];
 
 
+
     public FlyingFishView(Context context) {
         super(context);
-        fish = BitmapFactory.decodeResource(getResources(),R.drawable.fish1);
+        fish[0] = BitmapFactory.decodeResource(getResources(),R.drawable.fish1);
+        fish[1] = BitmapFactory.decodeResource(getResources(),R.drawable.fish2);
+
         backgroundImage = BitmapFactory.decodeResource(getResources(),R.drawable.background);
         scorePaint.setColor(Color.WHITE);
         scorePaint.setTextSize(70);
@@ -35,8 +42,24 @@ public class FlyingFishView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvasWidth = canvas.getWidth();
+        canvasHeight = canvas.getHeight();
+
+        int minFishY = fish[0].getHeight();
+        int maxFishY = canvasHeight - fish[0].getHeight()*3;
+        fishY = fishY * fishSpeed;
+
+        if(fishY < minFishY) {
+            fishY = minFishY;
+        }
+
+        if(fishY > maxFishY) {
+            fishY = maxFishY;
+        }
+
+
         canvas.drawBitmap(backgroundImage, 0 ,0, null);
-        canvas.drawBitmap(fish, 0 ,0, null);
+
         canvas.drawText("score",20,60,scorePaint);
         canvas.drawBitmap(life[0], 580 ,10, null);
         canvas.drawBitmap(life[0], 680 ,10, null);
